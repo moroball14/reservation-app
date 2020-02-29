e189d3ceb4667575cf22aa33faaeb42d211a0604
moroball14 <daiki.mr.0330@gmail.com>
2020-02-29 15:07:06 +0900

    サーバー動作確認のため、簡単なapiを作成

--- server/index.js
+++ server/index.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const app = express()
 
+app.get('/products', function(req, res) {
+  res.json({'success': true})
+})
+
 const PORT = process.env.PORT || '3001'
 
-app.listen('3001', function(){
+app.listen(PORT, function(){
   console.log('i am running!');
 })
