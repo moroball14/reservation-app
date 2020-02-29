6461d1d4d6a82025c4aad05a5cf590e6c74b36d3
moroball14 <daiki.mr.0330@gmail.com>
2020-02-29 16:09:42 +0900

    fake-dbの呼び出しとdbへデータ登録を実行

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const config = require('./config/dev')
+const config = require('./config/dev');
+const FakeDb = require('./fake-db');
 
 mongoose.connect(config.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+}).then(
+  () => {
+    const fakeDb = new FakeDb();
+    fakeDb.initDb()
+  }
+)
 
 
 const app = express()
