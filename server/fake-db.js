01c11f72187cd569e854d82585d19b2dbf2c3cf8
moroball14 <daiki.mr.0330@gmail.com>
2020-02-29 16:09:14 +0900

    dbのサンプルデータとそれらを登録する処理を記述

---
+++ server/fake-db.js
@@ -0,0 +1,80 @@
+const Product = require('./model/product')
+
+class FakeDb {
+  constructor(){
+    this.products = [
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'ヘッディング１',
+        heading2: 'ヘッディング２',
+        heading3: 'ヘッディング３',
+        headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: 'ヘッディング１',
+        heading2: 'ヘッディング２',
+        heading3: 'ヘッディング３',
+        headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heading1: 'ヘッディング１',
+        heading2: 'ヘッディング２',
+        heading3: 'ヘッディング３',
+        headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      },
+      {
+        coverImage: './assets/img/phone-cover.jpg',
+        name: 'Phone Special',
+        price: 999,
+        description: '',
+        heading1: 'ヘッディング１',
+        heading2: 'ヘッディング２',
+        heading3: 'ヘッディング３',
+        headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+      }
+    ]
+  }
+
+  async initDb(){
+    await this.cleanDb()
+    this.pushProductsToDb()
+  }
+
+  async cleanDb(){
+    await Product.deleteMany()
+  }
+
+  pushProductsToDb(){
+    this.products.forEach(
+      (product) => {
+        const newProduct = new Product(product)
+        newProduct.save()
+      }
+    )
+  }
+
+  seeDb(){
+    this.pushProductsToDb();
+  }
+}
+
+module.exports = FakeDb
