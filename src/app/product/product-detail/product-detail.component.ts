4c93324b518533e87ff95995544f00cd97ff89f0
moroball14 <daiki.mr.0330@gmail.com>
2020-03-01 13:36:08 +0900

    商品詳細情報をdbから取得

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { products } from '../../products'
 import { ProductService } from '../shared/product.service';
 
 @Component({
@@ -19,8 +18,17 @@ export class ProductDetailComponent implements OnInit {
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      // this.product = products[+params.get('productId')];
-      this.product = this.productService.getProductById(+params.get('productId'));
+      // this.product = this.productService.getProductById(params.get('productId'));
+      const productObservable = this.productService.getProductById(params.get('productId'));
+
+      productObservable.subscribe(
+        (data) => {
+          this.product = data
+        },
+        (err) => {
+
+        }
+      )
     });
   }
 
