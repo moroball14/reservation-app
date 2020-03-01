bad21e30a3be07e2e8f6b80b58a86f4c20fa2b6b
moroball14 <daiki.mr.0330@gmail.com>
2020-03-01 13:09:52 +0900

    観測対象を作成

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/product.service';
-import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-product-listings',
@@ -16,23 +16,31 @@ export class ProductListComponent implements OnInit {
   ngOnInit(): void {
     this.products = this.productService.getProducts();
 
-    const observable = new Observable(subscriber => {
-      subscriber.next(1);
-      subscriber.next(2);
-      subscriber.next(3);
-      setTimeout(() => {
-        subscriber.next(4);
-        subscriber.complete();
-      }, 1000);
-    });
+    const producctsObservable = this.productService.getProducts()
 
-    console.log('just before subscribe');
-    observable.subscribe({
-      next(x) { console.log('got value ' + x); },
-      error(err) { console.error('something wrong occurred: ' + err); },
-      complete() { console.log('done'); }
-    });
-    console.log('just after subscribe');
+    producctsObservable.subscribe(
+      (data) => { console.log('次のデータが出力されました:' + data) },
+      (err) => { console.error('次のエラーが発生しました:' + err) },
+      () => { console.log('完了しました。') }
+    )
+
+    // const observable = new Observable(subscriber => {
+    //   subscriber.next(1);
+    //   subscriber.next(2);
+    //   subscriber.next(3);
+    //   setTimeout(() => {
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // console.log('just before subscribe');
+    // observable.subscribe({
+    //   next(x) { console.log('got value ' + x); },
+    //   error(err) { console.error('something wrong occurred: ' + err); },
+    //   complete() { console.log('done'); }
+    // });
+    // console.log('just after subscribe');
 
   }
 
