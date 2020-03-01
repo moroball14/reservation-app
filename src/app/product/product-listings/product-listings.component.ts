71ed12e5274c24b218b67f3279e666ce1e2e7ef6
moroball14 <daiki.mr.0330@gmail.com>
2020-03-01 12:56:15 +0900

    observableのimport

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../products'
+import { ProductService } from '../shared/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-listings',
@@ -10,10 +11,29 @@ export class ProductListComponent implements OnInit {
 
   products: any;
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.products = products;
+    this.products = this.productService.getProducts();
+
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(x) { console.log('got value ' + x); },
+      error(err) { console.error('something wrong occurred: ' + err); },
+      complete() { console.log('done'); }
+    });
+    console.log('just after subscribe');
+
   }
 
 }
