c28af973c8dc8ec6b89929d05e55c4b23bca8d90
moroball14 <daiki.mr.0330@gmail.com>
2020-03-01 12:55:29 +0900

    productserviceの適用

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../../products'
+import { ProductService } from '../shared/product.service';
 
 @Component({
   selector: 'app-product-detail',
@@ -11,11 +12,15 @@ export class ProductDetailComponent implements OnInit {
 
   product
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
-      this.product = products[+params.get('productId')];
+      // this.product = products[+params.get('productId')];
+      this.product = this.productService.getProductById(+params.get('productId'));
     });
   }
 
