ff9179a7bbd9954786b06489ea4fdfd483706493
moroball14 <daiki.mr.0330@gmail.com>
2020-03-01 13:38:24 +0900

    dbのproductidを取得

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -24,27 +24,8 @@ export class ProductListComponent implements OnInit {
       },
       (err) => {
         console.error('次のエラーが発生しました:' + err)
-      },
-      () => { console.log('完了しました。') }
+      }
     )
-
-    // const observable = new Observable(subscriber => {
-    //   subscriber.next(1);
-    //   subscriber.next(2);
-    //   subscriber.next(3);
-    //   setTimeout(() => {
-    //     subscriber.next(4);
-    //     subscriber.complete();
-    //   }, 1000);
-    // });
-
-    // console.log('just before subscribe');
-    // observable.subscribe({
-    //   next(x) { console.log('got value ' + x); },
-    //   error(err) { console.error('something wrong occurred: ' + err); },
-    //   complete() { console.log('done'); }
-    // });
-    // console.log('just after subscribe');
 
   }
 
